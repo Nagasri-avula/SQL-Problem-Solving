676d13fa9b9d6fa5a3a5ecf1f4c174e95aca2aab
Nagasri-avula <avulanagasiri@gmail.com>
2026-08-27 16:07:06 +0530

    Time: 399 ms (37.74%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
-with s as(select name ,managerid , count(managerid) over(partition by managerid) as ct from employee),
-m as(select distinct managerid from s where ct>=5 )
-select e.name from employee e join m on m.managerid=e.id;
+with s as(select MANAGERID,count(managerid) over(partition by managerid) as d from employee)
+
+select e.name from employee e join S ON S.MANAGERID=E.ID WHERE D>=5
+GROUP BY E.ID,E.NAME;
